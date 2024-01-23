d7f1e08e04c9a034fa77828aaa8bef142acb564e
Ignacio Benito Soto <ignaciobenitosoto@gmail.com>
2024-01-23 13:04:14 +0100

    achieved auto scroll with javascript interop

---
+++ ScrollInterop.cs
@@ -0,0 +1,19 @@
+﻿namespace Boda_BlazorWASM_PowerAutomate
+{
+    using Microsoft.JSInterop;
+    using System.Threading.Tasks;
+
+    public static class ScrollInterop
+    {
+        public static ValueTask ScrollToBottom(this IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeVoidAsync("window.scrollToBottom");
+        }
+
+        public static ValueTask ScrollByPixels(this IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeVoidAsync("window.scrollByPixels");
+        }
+    }
+
+}
